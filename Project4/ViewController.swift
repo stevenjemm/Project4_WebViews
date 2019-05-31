ef6ddc5d7a7e149b49635c291cd1ff06c9d4c9ef
Steven Jemmott <jemmott.steven@gmail.com>
2019-05-31 14:39:17 -0400

    Include action sheet and hiding navBar on scrolling

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
     var webView: WKWebView!
     
@@ -22,13 +22,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        navigationController?.hidesBarsOnSwipe = true
         let url = URL(string: "https://www.hackingwithswift.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
 
-}
-
-extension ViewController: WKNavigationDelegate {
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage(action:)))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        
+        present(ac, animated: true)
+    }
     
+    @objc func openPage(action: UIAlertAction) {
+        
+        guard let actionTitle = action.title, let url = URL(string: "https://" + actionTitle) else { return }
+        webView.load(.init(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 }

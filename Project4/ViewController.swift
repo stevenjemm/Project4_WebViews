36f229701856a2567941680e9577d1d0a2ee7ff8
Steven Jemmott <jemmott.steven@gmail.com>
2019-05-31 14:26:43 -0400

    Loaded webView with url in loadView method

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
-
 
 }
 
+extension ViewController: WKNavigationDelegate {
+    
+}
